2caea3770ad57015b5feddfa04a64e29a843730b
Dmitry Zhuk <dmitryzhuk98@gmail.com>
2019-03-08 20:08:33 +0300

    Initial commit

---
+++ src/main/java/com/zhukdi/yourphotos/YourPhotosApplication.java
@@ -0,0 +1,11 @@
+package com.zhukdi.yourphotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourPhotosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YourPhotosApplication.class, args);
+    }
+}
